6a069910002d9eb8b1dc5fb49d6b02146b210764
munene19 <munenexv@gmail.com>
2020-02-05 10:12:54 +0300

    deployment to heroku

--- PhotoGallery/settings.py
+++ PhotoGallery/settings.py
@@ -19,21 +19,21 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
+# if config('MODE')=="dev":
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': config('DB_NAME'),
+#            'USER': config('DB_USER'),
+#            'PASSWORD': config('DB_PASSWORD'),
+#            'HOST': config('DB_HOST'),
+#            'PORT': '',
+#        }
        
-   }
+#    }
 # production
-else:
-   DATABASES = {
+# else:
+DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
        )
@@ -106,7 +106,14 @@ WSGI_APPLICATION = 'PhotoGallery.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picturess',
+        'USER': 'mike',
+        'PASSWORD': '12345',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
